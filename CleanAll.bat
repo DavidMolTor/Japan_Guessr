2792e0c1833f1a7afa44063bb4f1596bf1e31d43
David Molina <david.molinatoro@outlook.com>
2020-12-08 18:40:58 +0100

    Icon and project configuration
    Updated project intermediate output path.
    Added application icon.

--- CleanAll.bat
+++ CleanAll.bat
@@ -1,11 +1,5 @@
-RMDIR "./bin/Debug_x64" /S /Q
-RMDIR "./bin/Debug_x86" /S /Q
-RMDIR "./bin/Debug_AnyCPU" /S /Q
-RMDIR "./bin/Release_x64" /S /Q
-RMDIR "./bin/Release_x86" /S /Q
-RMDIR "./bin/Release_AnyCPU" /S /Q
+RMDIR "./bin/Debug" /S /Q
+RMDIR "./bin/Release" /S /Q
 
-RMDIR "./obj/Debug_x64" /S /Q
-RMDIR "./obj/Debug_x86" /S /Q
-RMDIR "./obj/Release_x64" /S /Q
-RMDIR "./obj/Release_x86" /S /Q
+RMDIR "./obj/Debug" /S /Q
+RMDIR "./obj/Release" /S /Q
